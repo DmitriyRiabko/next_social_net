6877c3a6036d6327d9014f2a1048a484f821a41e
DmitriyRiabko <riabko97@gmail.com>
2024-04-04 23:51:28 +0300

    added db_echo setting

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_url:str = "postgresql+asyncpg://admin:admin@localhost/next_social_net_db"
+    db_echo:bool = True
     
     
 settings = Settings()
