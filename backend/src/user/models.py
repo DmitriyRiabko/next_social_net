73a7f254d9d30dff1cd9d292ecb17590fc135e6f
DmitriyRiabko <riabko97@gmail.com>
2024-04-04 19:09:06 +0300

    added User model

--- backend/src/user/models.py
+++ backend/src/user/models.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
 from typing import Optional
 
-from src.database import Base
+from database import Base
 
 
 
