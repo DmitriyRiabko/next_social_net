88430c7a66c6bec71f889a5292a74d2c23a45b2a
DmitriyRiabko <riabko97@gmail.com>
2024-04-07 00:36:08 +0300

    delete user endpoint

--- backend/src/user/router.py
+++ backend/src/user/router.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import status
 from typing import List, Annotated
 
 
+from auth.oAuth2 import get_current_user
 from database import get_async_session
 from user import crud
 from user.schemas import UserRead, UserBase
+from auth.schemas import UserAuth
 
 router = APIRouter(
     prefix='/user',
@@ -28,9 +30,15 @@ async def create_user(user:UserBase, db:AsyncSession= Depends(get_async_session)
     return await crud.create_user(db,user)
 
 
-@router.get(
-    '/get', 
-    status_code=status.HTTP_200_OK,
-    response_model=UserRead | None)
-async def get_user_by_username(username:str ,db:AsyncSession= Depends(get_async_session)):
-    return await crud.get_user_by_username(db,username)
+@router.delete(
+    '/{user_id}', 
+    status_code=status.HTTP_204_NO_CONTENT
+    )
+async def delete_user(
+    user_id:int ,
+    db:AsyncSession= Depends(get_async_session),
+    current_user: UserAuth = Depends(get_current_user)
+    ):
+    actual_user = await current_user
+    
+    return await crud.delete_user(db,user_id, actual_user.id)
