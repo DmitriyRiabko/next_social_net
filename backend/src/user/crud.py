06bbc9a0a72d76477cfe5569f95dfd6d53882940
DmitriyRiabko <riabko97@gmail.com>
2024-04-07 00:35:27 +0300

    added delte user feature

--- backend/src/user/crud.py
+++ backend/src/user/crud.py
@@ -48,3 +48,24 @@ async def get_user_by_username(db:AsyncSession, username:str):
         )
         
     return user
+
+
+async def delete_user(db:AsyncSession, user_id:int, current_user_id:int):
+    query = select(User).where(User.id == user_id)
+    res = await db.execute(query)
+    user = res.scalar_one_or_none()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User not found'
+        )
+        
+    if user_id != current_user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Only owner can delete own account'
+        )
+    await db.delete(user)
+    await db.commit()
+    return 'deleted'
+
